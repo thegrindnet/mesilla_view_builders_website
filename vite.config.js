00dd85c615c2b785568b196de1dd761855babe38
David Bonilla <thegrindnet@gmail.com>
2026-06-12 13:30:03 -0600

    Update vite.config.js base so that boths slashes are in

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/mesilla_view_builders_website",
+  base: "/mesilla_view_builders_website/",
   server: {
     port: 3000,
   },
