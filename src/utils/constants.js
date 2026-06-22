e6a70b232f7a29eefc6fe25cb686947756d10187
David Bonilla <thegrindnet@gmail.com>
2026-06-22 17:19:24 -0600

    Upload new optimized photos and put 3 images in gallery

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -9,33 +9,18 @@ export const services = [
 
 export const galleryItems = [
   {
-    title: "Custom Home Framing",
+    title: "Luxury Walk-In Shower Installation",
     category: "Residential",
-    url: new URL("../assets/Shower1.jpg", import.meta.url).href,
+    url: new URL("../assets/bathroom_1.webp", import.meta.url).href,
   },
   {
-    title: "Concrete & Foundation Work",
-    category: "Foundation",
-    url: new URL("../assets/Steak.jpg", import.meta.url).href,
+    title: "Open Concept Living Space",
+    category: "",
+    url: new URL("../assets/large_bedroom_1.webp", import.meta.url).href,
   },
   {
-    title: "Commercial Buildout",
-    category: "Commercial",
-    url: new URL("../assets/Shower2.jpg", import.meta.url).href,
-  },
-  {
-    title: "Roofing & Exterior Finish",
-    category: "Exterior",
-    url: new URL("../assets/Bedroom1.jpg", import.meta.url).href,
-  },
-  {
-    title: "Interior Remodeling",
-    category: "Remodeling",
-    url: new URL("../assets/Apparrel.jpg", import.meta.url).href,
-  },
-  {
-    title: "Site Preparation",
-    category: "Project Prep",
-    url: new URL("../assets/Nature.jpg", import.meta.url).href,
+    title: "Custom Kitchen Remodel",
+    category: "",
+    url: new URL("../assets/kitchen_1.webp", import.meta.url).href,
   },
 ];
